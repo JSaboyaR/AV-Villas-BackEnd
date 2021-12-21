278b1ebfcecf2f98bfa25b36858052de72534b04
matorresUN <migueltorres@uninorte.edu.co>
2021-12-20 23:15:34 -0500

    rutas2

--- routes/Rutas.js
+++ routes/Rutas.js
@@ -1,11 +1,58 @@
 const express = require('express')
 
-const Usuarios = require('../models/Usurios')
-const Cuentas = require('../models/Cuentas')
-const Quejas = require('../models/Quejas')
-const Solicitudes = require('../models/Solicitudes')
-const Transacciones = require('../models/Transacciones')
+const Usuario = require('../models/Usurios')
+const Cuenta = require('../models/Cuentas')
+const Queja = require('../models/Quejas')
+const Solicitud = require('../models/Solicitudes')
+const Transaccion = require('../models/Transacciones')
 
 const rutas = express.Router()
+
+const usuarios = async () => {
+    const usuarios = await Usuario.find()
+    return usuarios
+}
+
+const cuentas = async () => {
+    const cuentas = await Cuenta.find()
+    return cuentas
+}
+
+const quejas = async () => {
+    const quejas = await Queja.find()
+    return quejas
+}
+
+const solicitudes = async () => {
+    const solicitudes = await Solicitud.find()
+    return solicitudes
+}
+
+const transacciones = async () => {
+    const transacciones = await Transaccion.find()
+    return transacciones
+}
+
+
+rutas.get('/get_usuarios', async (req, res) => {
+    res.json(await usuarios())
+})
+
+rutas.get('/get_cuentas', async (req, res) => {
+    res.json(await cuentas())
+})
+
+rutas.get('/get_quejas', async (req, res) => {
+    res.json(await quejas())
+})
+
+rutas.get('/get_solicitudes', async (req, res) => {
+    res.json(await solicitudes())
+})
+
+rutas.get('/get_transacciones', async (req, res) => {
+    res.json(await transacciones())
+})
+
 
 module.exports = rutas
--- routes/RutasPublicas.js
+++ routes/RutasPublicas.js
@@ -6,5 +6,56 @@ const Usuario = require('../models/Usuario')
 
 const rutas = express.Router()
 
+rutas.post('/crear_usuairo', async (req, res) => {
+    let datos_usuario = req.body
+
+    let salt = await bcrypt.genSalt(10)
+    let password = await bcrypt.hash(datos_usuario.pass, salt)
+
+    datos_usuario = {
+        ...datos_usuario,
+        pass: password
+    }
+
+    let usuario = new Usuario(datos_usuario)
+    await usuario.save()
+
+    res.json({mensaje: "usuario creado correctamente"})
+})
+
+rutas.post('/login', async (req, res) => {
+
+    let usuario = req.body.usuario
+
+    let usuario_bd = await Usuario.findOne({usuario: usuario})
+
+    if (!usuario_bd) {
+        return res.json({
+            mensaje: 'el usuario no exite'
+        })
+    } else {
+        let password = req.body.pass
+
+        let validar_password = await bcrypt.compare(password, usuario_bd.pass)
+
+        if (!validar_password) {
+            return res.json({
+                mensaje: 'clave invalida'
+            })
+        }
+    }
+
+    token = jwt.sign({
+        id: usuario_bd._id,
+        usuario: usuario_bd.usuario
+    }, process.env.SECRETO_JWT)
+
+    return res.json({
+        mensaje: 'Bienvenido',
+        usuario: usuario_bd,
+        token
+    })
+
+})
 
 module.exports = rutas
